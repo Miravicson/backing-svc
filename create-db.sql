3b5c9e216f44984d8509000af44a75d4a0a7a50f
Victor Chiagozie Ughonu <victorughonu95@gmail.com>
2024-01-22 15:38:12 +0530

    updated the backing service

--- create-db.sql
+++ create-db.sql
@@ -2,3 +2,6 @@ CREATE DATABASE dot_insure;
 CREATE DATABASE "dotpay-transactions";
 CREATE DATABASE "dotpay-transfers";
 CREATE DATABASE "dotpay_auth";
+CREATE DATABASE "hello_dev";
+CREATE DATABASE "test_database";
+-- CREATE DATABASE "getaways_dev";
